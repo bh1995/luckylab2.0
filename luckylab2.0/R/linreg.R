a504a4632639f7307cb718236bdb72ac3805f014
codercola-mw <meconho@gmail.com>
2019-09-24 12:10:31 +0200

    "linreg draft"

---
+++ luckylab2.0/R/linreg.R
@@ -0,0 +1,60 @@
+#' QR decoposition linear Regression.
+#' 
+#' @param formula A variable containing in the dataframe.
+#' @param data A data frame
+#' @return An S3 object of class linreg with QR decoposition 
+#' @export
+#' 
+linreg <- function(formula, data){
+  X<- model.matrix(formula,data)
+  y<- data[,all.vars(formula)[1]]
+  
+  #QR decomposition
+  QR <- function(X){
+    # Empty U matrix
+    U <- matrix(0,dim(X)[1],dim(X)[2])
+    
+    # Fill U matrix
+    for (i in 1:(dim(X)[2])){
+      U[,i] <- cbind(X[,i]) 
+      if (dim(X)[2]>1){
+        for (j in 1:(dim(X)[2]-1)){
+          if (j+1>i) {break}
+          U[,i] <- U[,i] - as.numeric(t(U[,j])%*%X[,i]/t(U[,j])%*%U[,j])*U[,j]
+        }
+      }
+    }
+    
+    # Calculate the e-values and put them in the Q matrix.
+    Q <- apply(U, 2, function(x)(x/sqrt((sum(x^2)))))
+    
+    # Empty R matrix
+    R <- matrix(0,dim(X)[2],dim(X)[2])
+    
+    # Fill R matrix
+    for (i in 1:(dim(X)[2])){
+      # Start j from (dim(X)[2]) so that lower triangular of R = 0
+      for (j in (dim(X)[2]):1){
+        if (j<i) {break}
+        R[i,j] <- t(Q[,i])%*%X[,j]
+      }
+    }
+    return(list(Q,R))
+  }
+  
+  
+  multreg <- function(Q, R, y){
+      b <- (solve(R)) %*% t(Q) %*% y
+      fitted <- Q%*%R%*%b
+      resid <- y - fitted
+      df <- nrow(Q%*%R)-ncol(Q%*%R)
+      residvar <- t(resid)%*%resid/df
+      varb <- solve(t(R)%*%R) * as.numeric(residvar)
+      t <- b/sqrt(diag(varb))
+      pval <- 1-pt(t,df)
+  
+    
+      
+      
+  }
+}
