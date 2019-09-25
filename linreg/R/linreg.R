445d6cb4962f5fc9cf2be665fcf74c26240cce5a
Mahbuba Haque <mahbuba.haque@ki.se>
2019-09-25 11:45:47 +0200

    "edited param return"

--- linreg/R/linreg.R
+++ linreg/R/linreg.R
@@ -1,8 +1,8 @@
-#' QR decoposition linear Regression.
+#' Linear Regression.
 #' 
-#' @param formula A variable containing in the dataframe.
-#' @param data A data frame
-#' @return An S3 object of class linreg with QR decoposition 
+#' @param formula A object of class "formula".
+#' @param data A data frame.
+#' @return An S3 object of class linreg.
 #' @export
 #' 
 linreg <- function(formula, data){
@@ -42,45 +42,42 @@ linreg <- function(formula, data){
     return(list(Q,R))
   }
   
-Q <- QR(X)[[1]]  
-R <- QR(X)[[2]]
+  Q <- QR(X)[[1]]  
+  R <- QR(X)[[2]]
 
   multreg <- function(Q, R, y){
       b <- (solve(R)) %*% t(Q) %*% y
       fitted <- Q%*%R%*%b
       resid <- y - fitted
-      df <- nrow(Q%*%R)-ncol(Q%*%R)
+      df <- nrow(Q)-ncol(Q)
       residvar <- t(resid)%*%resid/df
       varb <- solve(t(R)%*%R) * as.numeric(residvar)
       t <- b/sqrt(diag(varb))
       pval <- 1-pt(t,df)
       return(list(b, fitted, resid, df, residvar, varb, t, pval))
   }
-
-
-output <- multreg(Q, R, y)
-
-coeff <- c(output[[1]])
-names(coeff) <- colnames(X)
-reg <- list(formula, coeff, output[[2]], output[[3]], output[[6]], output[[7]], output[[8]], output[[4]])
-names(reg) <- c("formula","coefficients","fitted","residuals","varcoef","t-values","p-values","df")
-class(reg) <- "linreg" 
-return(reg)
-
+  
+  output <- multreg(Q, R, y)
+  
+  coeff <- c(output[[1]])
+  names(coeff) <- colnames(X)
+  reg <- list(formula, coeff, output[[2]], output[[3]], output[[6]], output[[7]], output[[8]], output[[4]])
+  names(reg) <- c("formula","coefficients","fitted","residuals","varcoef","t-values","p-values","df")
+  class(reg) <- "linreg" 
+  return(reg)
 }
 
 #' Residuals
 #'
-#' \code{plot.linreg} Generate residual plots of a linear regression model
+#' \code{plot.linreg} Returns the vector of residuals from a linear regression model.
 #'
-#' This function returns a vector of residuals from a linear regression stored in 
+#' This function returns the vector of residuals from a linear regression stored in 
 #' an S3 object of class linreg.
 #'
 #' @export
-#' @param x An object of class linreg contanining a linear regression.
-#' @return A numeric vector of residuals 
+#' @param x An object of class linreg containing a linear regression.
+#' @return A numeric vector of residuals. 
 resid <- function(x){UseMethod("resid",x)}
-
 #' @export
 resid.linreg <- function(x){
   return(x$residuals)
@@ -89,39 +86,32 @@ resid.linreg <- function(x){
 
 #' Fitted values
 #'
-#' \code{plot.linreg} Return fitted values from a linear regression model
+#' \code{plot.linreg} Returns predicted values from a linear regression model.
 #'
-#' This function returns the fitted values of a linear regression stored in 
+#' This function returns the predicted values of a linear regression stored in 
 #' an S3 object of class linreg.
 #'
 #' @export
-#' @param x An object of class linreg contanining a linear regression.
-#' @return A numeric vector of fitted values
+#' @param x An object of class linreg containing a linear regression.
+#' @return A numeric vector of predicted values.
 pred <- function(x){UseMethod("pred",x)}
-
-
-
-
 #' @export
 pred.linreg <- function(x){
   return(x$fitted)
 }
 
+
 #' Regression coefficients
 #'
-#' \code{plot.linreg} Return estimated coefficients from a linear regression model
+#' \code{plot.linreg} Returns coefficients from a linear regression model.
 #'
-#' This function returns estimated coefficients of a linear regression stored in 
+#' This function returns the coefficients of a linear regression stored in 
 #' an S3 object of class linreg.
 #'
 #' @export
-#' @param x An object of class linreg contanining a linear regression.
-#' @return A numeric vector with the estimated coefficients
+#' @param x An object of class linreg containing a linear regression.
+#' @return A numeric vector of coefficients.
 coef <- function(x){UseMethod("coef",x)}
-
-
-
-
 #' @export
 coef.linreg <- function(x){
   return(x$coefficients)
