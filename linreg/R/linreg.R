c60958e1aab782a09f44c62e02229c0cd6b3646d
bh1995 <55191733+bh1995@users.noreply.github.com>
2019-09-27 17:29:53 +0200

    "fefef"

--- linreg/R/linreg.R
+++ linreg/R/linreg.R
@@ -52,9 +52,9 @@ linreg <- function(formula, data){
       df <- nrow(Q)-ncol(Q)
       residvar <- t(resid)%*%resid/df
       varb <- solve(t(R)%*%R) * as.numeric(residvar)
-      t <- b/sqrt(diag(varb))
-      pval <- 1-pt(t,df)
-      return(list(b, fitted, resid, df, residvar, varb, t, pval))
+      tval <- b/sqrt(diag(varb))
+      pval <- 1-pt(tval,df)
+      return(list(b, fitted, resid, df, residvar, varb, tval, pval))
   }
   
   output <<- multreg(Q, R, y)
@@ -66,21 +66,11 @@ linreg <- function(formula, data){
   names(coeff) <- colnames(X)
   
   dataname <- deparse(substitute(data)) #for the print methods
-  reg <<- list(formula, coeff, output[[2]], output[[3]], output[[6]], output[[7]], output[[8]], output[[4]], dataname, X)
-  names(reg) <<- c("formula","coefficients","fitted","residuals","varcoef","t-values","p-values","df", "dataname", "X")
+  reg <<- list(formula, coeff, output[[2]], output[[3]], output[[6]], output[[7]], output[[8]], output[[4]], dataname, n)
+  names(reg) <<- c("formula","coefficients","fitted","residuals","varcoef","t-values","p-values","df", "dataname", "n")
   class(reg) <<- "linreg" 
   return(reg)
 }
-
-#' Regression summary
-#'
-#' \code{plot.linreg} Outouts a summary of the calculated results from the regression model
-#'
-#' @export
-#' @param x Which is an object of class linreg
-#' @return summary of calculated values
-summary <- function(x){UseMethod("summary",x)}
-
 
 #' Print
 #'
@@ -97,23 +87,17 @@ print <- function(x){UseMethod("print",x)}
 print.linreg <- function(x){
   a <- as.character(x$dataname)
   b <- as.character(x$formula)
-  printformula <- function(x){
-    
-  }
-  c <- printformula(x)
+  
   # cat("Call:\n", "linreg(", c )
   # 
   # cat(")",",","data =", a, "\n","Coefficients:\n")
   
-  x$coefficients
-  
-  a <- as.character(x$dataname)
-  b <- format(x$formula)
   cat("Call:")
   cat("\n")
-  formula_print<- paste0("linreg(formula = ","",b,","," data = ","",a,")","\n","\n","Coefficients:\n", sep=" " )
-  cat(formula_print)
-  round(x$coefficients, digits=3)
+  # formula_print<- paste0("linreg(formula = ","",b,","," data = ","",a ,")","\n","\n","Coefficients:\n", sep=" " )
+  # cat(formula_print)
+  # round(x$coefficients, digits=3)
+  # x$coefficients
 }
 # ***test <- linreg(formula, iris) <- must enter "iris" instead of "data"***
 
@@ -170,5 +154,32 @@ coef.linreg <- function(x,...){
 }
 
 
+#' Regression summary
+#'
+#' \code{summary.linreg} Outouts a summary of the calculated results from the regression model
+#'
+#' @export
+#' @param x Which is an object of class linreg
+#' @return summary of calculated values
+summary <- function(x){UseMethod("summary",x)}
 
+#' @export
+summary.linreg <- function(x){
+  printSum <- function(b, bvar, tval, pval, n){
+    cat(sprintf("Coefficients\t\tSdError\t\tTvalue\t\tPvalue\n"))
+  for (i in 1:n){
+    
+    cat(sprintf("%-15s\t\t%6.6f\t%6.6f\t%6.6f\n",strtrim(names(b[i]),15),bvar[i],tval[i],pval[i]))
+  }
+  cat(sprintf("Estimated error variance: %f\n",var(x$resid)))
+  cat(sprintf("Degrees of freedom: %d\n",x$df))
+  
+}
+  n <- length(x$coefficients)
+  coeff <- x$coefficients
+  bvar <- x$bvar
+  T <- x$tval
+  P <- x$pval
+  printSummary(coefficients,bvar,T,pval,n)
+}
 
